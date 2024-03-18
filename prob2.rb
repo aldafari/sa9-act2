10ffa1294e81b9a2c00f5cbe5ca90fe9b8cf04fa
aldafari <aldafari@memphis.edu>
2024-03-17 23:11:14 -0500

    Completed problem 2.

--- prob2.rb
+++ prob2.rb
@@ -1,19 +1,22 @@
 RSpec.describe TodoList do
   describe "#add" do
     it "adds a todo to the list" do
-      # Fill in the test
+      todos.add
+      expect(todos).to be += todos
     end
   end
 
   describe "#remove" do
     it "removes a todo from the list" do
-      # Fill in the test
+      todos.remove
+      expect(todos).to be -= todos
     end
   end
 
   describe "#todos" do
     it "returns all todos" do
-      # Fill in the test
+      todos.todos
+      expect(puts todos)
     end
   end
 end
